9c604a9f63495c0ac8c9fa3a19fc368c51ed5a0f
utsavkashyap5 <utsavkashyap892@gmail.com>
2026-07-14 22:06:50 +0530

    Opimized some services

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),
+     visualizer({
+      filename: "stats.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
 })
